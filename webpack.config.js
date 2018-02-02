ff3ef6afddefb8b7426c9ecad269ce4206f02e66
Petr Ptacek <petr.ptacek99@gmail.com>
2018-02-02 22:21:45 +0100

    Initialize the project for develop.

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: ["./src/index.tsx"],
+
+    output: {
+        path: path.resolve(__dirname, "build"),
+        filename: "main.js",
+        publicPath: "/build/"
+    },
+
+    resolve: {
+        extensions: [
+            ".ts", ".tsx", ".css", ".js", ".json"
+        ]
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css/,
+                use: ExtractTextPlugin.extract(
+                    {
+                        fallback: "style-loader",
+                        use:'app.css-loader'
+
+                    })
+            }
+        ]
+    },
+
+    plugins: [
+        new ExtractTextPlugin("main.css")
+    ]
+};
